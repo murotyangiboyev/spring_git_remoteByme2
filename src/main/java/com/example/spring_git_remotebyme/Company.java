0fe229734650303f78d99664b52ded2e234364de
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2025-11-08 11:42:08 +0500

    second commit

---
+++ src/main/java/com/example/spring_git_remotebyme/Company.java
@@ -0,0 +1,12 @@
+package com.example.spring_git_remotebyme;
+
+public class Company {
+
+    private String name;
+    private String address;
+
+    public Company(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
